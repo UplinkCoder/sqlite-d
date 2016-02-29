5db24a7b5588b873c6264079cdaab1ff2c7a64d7
Stefan Koch <uplink.coder@googlemail.com>
2016-02-29 18:28:19 +0000

    remove a bunch of stale example code

--- source/app.d
+++ source/app.d
@@ -9,83 +9,8 @@ import core.memory;
 import std.algorithm : map, filter, count;
 import std.range : join, takeOne;
 //import std.typecons : tuple;
-
-void* pageHandler(Database.BTreePage page, Database.PageRange pages, void* unused) {
-	string toPrint = page.header.to!string; //~ "\n" ~ page.toString(pages); 
-	writeln(toPrint);
-	return null;
-}
-
-void* countCellsHandler(Database.BTreePage page, Database.PageRange pages, void* currentCount) {
-	*cast(uint*)currentCount += page.header.cellsInPage;
-	return currentCount;
-}
-
 static immutable db4 = cast(immutable) Database(cast(ubyte[]) import("test4.s3db"), "");
 
-
-auto getTableNames(const Database db) {
-	import std.array : array;
-	return handleRow!((r) {
-			if (r.colums(0).getAs!string == "table") {
-				return r.colums(1).getAs!string;
-			} else {
-				return "";
-			}
-		}
-	)(db.rootPage, db.pages);
-}
-
-
-auto getRowsOf0(const Database db, const string tableName) {
-	return handleRow!(
-		(r) {
-			if (r.colums(0).getAs!string == "table" && 
-				r.colums(1).getAs!string == tableName) {
-				auto n = r.colums(3).getAs!uint - 1;
-				return handleRow!((_r) {
-					return _r;
-				}) (db.pages[n], db.pages);
-			} else {
-				return null;
-			}
-
-		})(db.rootPage, db.pages);
-}
-
-auto getRowsOf1(const Database db, const string tableName) {
-	auto n = getRootPageOf1(db, tableName);
-	return (n ? handleRow!(r => r)(db.pages[n], db.pages) : null);
-}
-
-
-auto getRootPageOf1(const Database db, const string tableName) {
-	uint rootPage;	
-	handleRow!(
-		(r) {
-			if (r.colums(0).getAs!string == "table" && 
-				r.colums(1).getAs!string == tableName) {
-				rootPage = r.colums(3).getAs!uint - 1;
-			}
-	})(db.pages[0], db.pages);
-
-
-	return rootPage;
-}
-
-auto getRootPageOf2(const Database db, const string tableName) {
-	uint rootPage;	
-	handleRow!(
-		(r) {
-			auto cols = r.colums(1,3);
-			if (cols[0].getAs!string == tableName) {
-				assert(rootPage == 0,"tableName duplicated");
-				rootPage = cols[1].getAs!uint - 1;
-			}
-		})(db.pages[0], db.pages);
-
-	return rootPage;
-}
 
 
 int main(string[] args) {
@@ -102,7 +27,7 @@ int main(string[] args) {
 	foreach(schema;schemas) {
 		writeln(schema);
 	}
-	writeln("rootPage of Towns:", test_db.getRootPageOf2("Towns"));
+
 	struct Town {
 		uint PK_UID;
 		string name;
@@ -116,7 +41,7 @@ int main(string[] args) {
 	towns = handleRow!(r => r.deserialize!Town)(test_db.table("Towns"));
 
 	foreach(town;towns) {
-		//writeln(town);
+		writeln(town);
 	}
 
 
@@ -124,23 +49,21 @@ int main(string[] args) {
 	//	writeln("it appears to be a database");
 		import std.datetime;
 		StopWatch sw;
+	uint x;
 		enum times = 2*4096;
 		foreach(_; 0 .. times) {
 			string result;
-		
+		x = 0;
 			sw.start;
-			auto x = result.length;
 			foreach(row;(db4).table("Album")) {
-			//	writeln(row.colums(1).getAs!string);
+			x++;
+				row.colums(1).getAs!string;
 			} 
 			sw.stop();
 		}
 
-		writeln("Getting all entries of colum 1 in table Album ", times, " times took ", sw.peek().msecs, "msecs");
+		writeln("Getting all ", x, " entries of colum 1 in table Album ", times, " times took ", sw.peek().msecs, "msecs");
 //	writeln(db4.table("Artist"));
-//	foreach(_;0..32) {
-//		writeln(comparingBenchmark!((){test_db.getRootPageOf1("Towns");},(){test_db.getRootPageOf2("Towns");},4096).point);
-//	}
 		return 0;
 //	} else {
 //		writeln("invalid database or header corrupted");
