0a1d458328e45404987d592baff0572280d237eb
Stefan Koch <uplink.coder@googlemail.com>
2016-03-02 11:24:32 +0000

    added superficial test for parseCreateTable

--- source/test.d
+++ source/test.d
@@ -10,3 +10,15 @@ q{CREATE TABLE `veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery
 static immutable test_s3db = cast(immutable)Database(cast(immutable ubyte[]) import("test.s3db"));
 static immutable Database.MasterTableSchema[] schemas = handleRow!(r => r.deserialize!(Database.MasterTableSchema))(test_s3db.rootPage, test_s3db.pages);
 static assert(schemas[2].sql==long_create_table);
+
+import frail_sql_parser;
+
+static assert (parseCreateTable(
+q{CREATE TABLE spatial_ref_sys (
+		srid INTEGER NOT NULL PRIMARY KEY,
+		auth_name VARCHAR(256) NOT NULL,
+		auth_srid INTEGER NOT NULL,
+		ref_sys_name VARCHAR(256),
+		proj4text VARCHAR(2048) NOT NULL)}
+	) != TableInfo.init);
+static assert(parseCreateTable(long_create_table) != TableInfo.init);
